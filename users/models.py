9f0d68b3b10687c2fa5349c1b8e1c881b125f80f
kneeraazon01 <kneeraazon@gmail.com>
2022-09-12 21:01:22 +0545

    first commit- templates changes with tailwind by nirajan

--- users/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class ExcelFile(models.Model):
-    excel_file = models.FileField(upload_to="zippedFiles/")
-
-    def __str__(self):
-        return self.name
--- users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.homeView, name="home"),
-    path("login/", views.loginView, name="login"),
-    path("file/", views.fileView, name="file"),
-]
